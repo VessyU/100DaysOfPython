aec36809b833a5de6847e2641f867c1f3ea79cb2
VessyU <vessy.ustabashiev@gmail.com>
2022-04-02 23:14:04 +0100

    Create main.py

---
+++ Day 1 - Band Name Generator/main.py
@@ -0,0 +1,5 @@
+# Write your code below this line 👇
+print("Hello " + input("What is your name? "))
+x = input("What is the name of the city you grew up in? ")
+y = input("What is your favourite animal? ")
+print("Hope you're not disappointed but...your band name is "+x+" "+y+"!!!")
